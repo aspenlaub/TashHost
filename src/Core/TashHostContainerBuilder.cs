a7283ca5cae0fd86324e168ecc2a5eca320229df
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-09 23:27:28 +0100

    Package/-s updated to most recent version

--- src/Core/TashHostContainerBuilder.cs
+++ src/Core/TashHostContainerBuilder.cs
@@ -1,12 +1,11 @@
 ﻿using Aspenlaub.Net.GitHub.CSharp.Dvin.Components;
-using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Autofac;
 
 namespace Aspenlaub.Net.GitHub.CSharp.TashHost.Core;
 
 public static class TashHostContainerBuilder {
     public static ContainerBuilder UseTashHost(this ContainerBuilder builder, string applicationName) {
-        builder.UseDvinAndPegh(applicationName, new DummyCsArgumentPrompter());
+        builder.UseDvinAndPegh(applicationName);
         return builder;
     }
 }
